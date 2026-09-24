7b2ea4f15f48d2f47c1ec790b820424ff79f3c23
Akkraizen <akkraizen@gmail.com>
2026-09-24 14:54:31 +0300

    update celebrity handlers

--- src/handler/celebrity_handler.py
+++ src/handler/celebrity_handler.py
@@ -1,24 +1,49 @@
-from re import RegexFlag
-
-from aiogram import Router, F
+import re
+from aiogram import Router
 from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.utils.markdown import italic
 
 router = Router(name="celebrity_router")
 
-@router.message(F.text.regexp(r"(поздняк|пиздняк|пиздяк)", search=True, flags=RegexFlag.IGNORECASE))
-async def pozdnyakov_handler(message: Message) -> None:
-    await message.reply(italic("*Поздняков признан ебланом на территории данного чата"), parse_mode=ParseMode.MARKDOWN_V2)
+CELEBRITIES = [
+    {
+        "pattern": re.compile(
+            r"\b(?:поздняк(?:ов(?:а|у|ом|е|ы|ов|ам|ами|ах)?)?|пиздняк(?:ов(?:а|у|ом|е|ы|ов|ам|ами|ах)?)?|пиздяк(?:ов(?:а|у|ом|е|ы|ов|ам|ами|ах)?)?)\b",
+            re.IGNORECASE,
+        ),
+        "text": "*Поздняков признан ебланом на территории данного чата",
+    },
+    {
+        "pattern": re.compile(
+            r"\bкац(?:а|у|ем|е|ы|ов|ам|ами|ах)?\b",
+            re.IGNORECASE,
+        ),
+        "text": "*Кац признан ебателем собак на территории данного чата",
+    },
+    {
+        "pattern": re.compile(
+            r"\bнаки\b",
+            re.IGNORECASE,
+        ),
+        "text": "*Майкл Наки признан сыном собаки на территории данного чата",
+    },
+    {
+        "pattern": re.compile(
+            r"\bварлам(?:ов(?:а|у|ом|е|ы|ов|ам|ами|ах)?)?\b",
+            re.IGNORECASE,
+        ),
+        "text": "*Илья Варламов признан ебателем кошек на территории данного чата",
+    },
+]
 
-@router.message(F.text.regexp(r"(кац)", search=True, flags=RegexFlag.IGNORECASE))
-async def katz_handler(message: Message) -> None:
-    await message.reply(italic("*Кац признан ебателем собак на территории данного чата"), parse_mode=ParseMode.MARKDOWN_V2)
 
-@router.message(F.text.regexp(r"(наки)", search=True, flags=RegexFlag.IGNORECASE))
-async def pozdnyakov_handler(message: Message) -> None:
-    await message.reply(italic("*Майкл Наки признан сыном собаки на территории данного чата"), parse_mode=ParseMode.MARKDOWN_V2)
+@router.message()
+async def celebrity_handler(message: Message) -> None:
+    if not message.text:
+        return
 
-@router.message(F.text.regexp(r"(варлам)", search=True, flags=RegexFlag.IGNORECASE))
-async def katz_handler(message: Message) -> None:
-    await message.reply(italic("*Илья Варламов признан ебателем кошек на территории данного чата"), parse_mode=ParseMode.MARKDOWN_V2)
+    for celeb in CELEBRITIES:
+        if celeb["pattern"].search(message.text):
+            await message.reply(italic(celeb["text"]), parse_mode=ParseMode.MARKDOWN_V2)
+            break
